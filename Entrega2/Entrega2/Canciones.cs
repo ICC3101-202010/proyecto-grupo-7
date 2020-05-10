4162089207d7fcb0b9b3cb1f41427fc681bde479
TomasBruner <tfbruner@miuandes.com>
2020-05-10 19:34:46 -0400

    avance 3

---
+++ Entrega2/Entrega2/Canciones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entrega2
+{
+    class Canciones
+    {
+        private string titulo;
+        private string genero;
+        private string discografia;
+        private string estudio;
+        private string letra;
+        private string premios;
+        private string cantante;
+        private string compositor;
+        private int calificacion;
+        private int añoPublicacion;
+
+    }
+}
