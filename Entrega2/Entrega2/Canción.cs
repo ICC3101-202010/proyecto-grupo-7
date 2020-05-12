3d30c0b1c035a8d558d6710a1a1a9d4e1863c84a
diegoriedel <62268951+diegoriedel@users.noreply.github.com>
2020-05-12 18:37:50 -0400

    Creado cancion

--- Entrega2/Entrega2/Canción.cs
+++ Entrega2/Entrega2/Canción.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,60 @@ namespace Entrega2
         public List<double> rating;
         public int numReproductions;
         public List<double> Rating { get => rating; set => rating = value; }
+    }
+    public Cancion(Album album,Artista artista, string titulo,string genero,string discografia,string estudio,string letra,string premios,string cantante, string compositor,int Avg_calificacion,int añoPublicacion, int numReproductions)
+    {
+        this.Titulo = titulo;
+        this.genero = genero;
+        this.discografia = discografia;
+        this.Estudio = estudio;
+        this.letra = letra;
+        this.premios = premios;
+        this.cantante = cantante;
+        this.compositor = compositor;
+        this.Avg_calificacion = Avg_calificacion;
+        this.añoPublicacion = añoPublicacion;
+        this.numReproductions = numReproductions;
+    }
+    public string Titulo
+    {
+        get => titulo; set => titulo= value;
+    }
+    public string genero
+    {
+        get => genero; set => genero = value;
+    }
+    public string discografia
+    {
+        get => discografia; set => discografia = value;
+    }
+    public string Estudio
+    {
+        get => estudio; set => estudio = value;
+    }
+    public string letra
+    {
+        get => letra; set => letra = value
+    }
+    public string premios
+    {
+        get => premios; set => premios = value;
+    }
+    public string cantante
+    {
+        get => cantante; set => cantante = value;
+    }
+    public string compositor
+    {
+        get => compositor; set => compositor = value;
+    }
+    public int añoPublicacion
+    {
+        get => añoPublicacion; set => añoPublicacion = value;
+    }
+    public int numReproductions
+    {
+        get => numReproductions; set => numReproductions = value;
     }
 
 }
