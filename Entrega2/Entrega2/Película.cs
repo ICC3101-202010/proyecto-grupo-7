624431488fc82a630ecf5f30eb5fc6f95e1ea970
diegoriedel <62268951+diegoriedel@users.noreply.github.com>
2020-05-12 15:25:38 -0400

    Borrado y arreglado pelicula

--- Entrega2/Entrega2/Película.cs
+++ Entrega2/Entrega2/Película.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entrega2
-{
+{     
     public class Película
     {
         public string titulo;
@@ -17,9 +18,68 @@ namespace Entrega2
         public int duracion;
         public int min;
         public int añoPublicacion;
-        public int calificacion;
+        public int clasificacion;
         public List<double> rating;
         public List<double> Rating { get => rating; set => rating = value; }
         public int Avg_Ranking;
+
+        public Película(string titulo, string genero, string categoria, string estudio, string descripcion, string premios, int duracion, int min,
+            int anoPublicacion, int clasificacion, List<double> rating, int Avg_Ranking)
+        {
+            this.Titulo = titulo;
+            this.Genero = genero;
+            this.Categoria = categoria;
+            this.Estudio = estudio;
+            this.Premios = premios;
+            this.Descripcion = descripcion;
+            this.duracion = duracion;
+            this.min = min;
+            this.AñoPublicacion = anoPublicacion;
+            this.clasificacion = clasificacion;
+            this.rating = rating;
+            this.Avg_Ranking = Avg_Ranking;
+        }
+
+        public string Titulo
+        {
+            get => titulo; set => titulo = Value;
+        }
+        public string Categoria
+        {
+            get =>categoria; set =>categoria = value;
+        }
+        public string Estudio
+        {
+            get => estudio; set => estudio = value;
+        }
+        public string Premios
+        {
+            get => premios; set => premios = value;
+        }
+        public string Descripcion
+        {
+            get => descripcion; set => descripcion = value;
+        }
+        public int duracion
+        {
+            get => duracion; set => duracion = value;
+        }
+        public int AnoPublicacion
+        {
+            get => anoPublicacion; set => anoPublicacion = value;
+        }
+        public int clasificacion
+        {
+            get => clasificacion; set => clasificacion = value;
+        }
+        public int rating
+        {
+            get => rating; set => rating = value;
+        }
+        public int Avg_Ranking
+        {
+            get => Avg_Ranking; set => Avg_Ranking = value;
+        }
     }
+
 }
