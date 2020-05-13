fb09b3e686c70c281d3102bdba0d5b316995d799
ignacioperrotta <ijperrotta@miuandes.cl>
2020-05-12 20:02:49 -0400

    ya

--- Entrega2/Entrega2/Película.cs
+++ Entrega2/Entrega2/Película.cs
@@ -17,35 +17,21 @@ namespace Entrega2
         public int duracion;
         public int min;
         public int añoPublicacion;
-<<<<<<< HEAD
         public int rating;
         public List<int> Rating;
         public int avg_Ranking;
         public List<Person> Actores;
 
-        public Película(string titulo, string categoria, string estudio, string descripcion, string premios, int duracion, int min,
-            int anoPublicacion, int rating, List<double> Rating, int avg_Ranking)
-        {
-            this.Titulo = titulo;
-=======
-        public int clasificacion;
-        public List<double> rating;
-        public List<double> Rating { get => rating; set => rating = value; }
-        public int Avg_Ranking;
-
         public Película(string titulo, string genero, string categoria, string estudio, string descripcion, string premios, int duracion, int min,
             int anoPublicacion, int clasificacion, List<double> rating, int Avg_Ranking)
         {
             this.Titulo = titulo;
-            this.Genero = genero;
->>>>>>> 3d30c0b1c035a8d558d6710a1a1a9d4e1863c84a
             this.Categoria = categoria;
             this.Estudio = estudio;
             this.Premios = premios;
             this.Descripcion = descripcion;
             this.duracion = duracion;
             this.min = min;
-<<<<<<< HEAD
             this.AñoPublicacion = añoPublicacion;
             this.Avg_Ranking = avg_Ranking;
         }
@@ -55,22 +41,7 @@ namespace Entrega2
         }
         public string Categoria
         {
-            get => categoria; set => categoria = value;
-=======
-            this.AñoPublicacion = anoPublicacion;
-            this.clasificacion = clasificacion;
-            this.rating = rating;
-            this.Avg_Ranking = Avg_Ranking;
-        }
-
-        public string Titulo
-        {
-            get => titulo; set => titulo = Value;
-        }
-        public string Categoria
-        {
             get =>categoria; set =>categoria = value;
->>>>>>> 3d30c0b1c035a8d558d6710a1a1a9d4e1863c84a
         }
         public string Estudio
         {
@@ -84,7 +55,6 @@ namespace Entrega2
         {
             get => descripcion; set => descripcion = value;
         }
-<<<<<<< HEAD
         public int Duracion
         {
             get => duracion; set => duracion = value;
@@ -106,29 +76,6 @@ namespace Entrega2
                 Console.WriteLine(actores.Nombre);
             }
         }
-       
-=======
-        public int duracion
-        {
-            get => duracion; set => duracion = value;
-        }
-        public int AnoPublicacion
-        {
-            get => anoPublicacion; set => anoPublicacion = value;
-        }
-        public int clasificacion
-        {
-            get => clasificacion; set => clasificacion = value;
-        }
-        public int rating
-        {
-            get => rating; set => rating = value;
-        }
-        public int Avg_Ranking
-        {
-            get => Avg_Ranking; set => Avg_Ranking = value;
-        }
->>>>>>> 3d30c0b1c035a8d558d6710a1a1a9d4e1863c84a
     }
 
 }
